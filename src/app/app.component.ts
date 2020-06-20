4b32f472cfcac91ec7c69d71814f2a3bed22ff62
Ayush Chasta <chasta.ayush@gmail.com>
2020-06-20 17:30:17 +0530

    -> Adds More Book Options
    	-> Shows Uploader Name
    	-> Shows Uploader Id
    	-> Shows Views
    	-> Shows Downloads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'DigitalLibrary-Frontend';
+    title = 'DigitalLibrary-Frontend';
 }
